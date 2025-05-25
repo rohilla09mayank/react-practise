dc696bd0b418521dc138dfa25c49621e5b2e3cd5
MayankRohilla17299 <52850405+MayankRohilla17299@users.noreply.github.com>
2025-05-25 14:17:11 +0530

    Completed

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -98,12 +98,12 @@ function Menu() {
 
 function Pizza(props) {
   return (
-    <li className={!props.soldOut ? "pizza" : "pizza sold-out"}>
+    <li className={`${!props.soldOut ? "" : "sold-out"} pizza`}>
       <img src={props.photoName} alt={props.name} />
       <div>
         <h3>{props.name}</h3>
         <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <span>{props.soldOut ? "SOLD OUT" : props.price}</span>
       </div>
     </li>
   );
